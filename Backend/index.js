a6c8ceabad385f73a982cfb6785880c19ed7ccc3
Hemant Mooneea <hemantmooneea1@gmail.com>
2024-09-25 12:22:31 +0400

    create a default route to test the server

--- Backend/index.js
+++ Backend/index.js
@@ -3,3 +3,17 @@ import path from "path";
 import { MongoDatabase } from "./mongodb.js";
 
 const MongoDB = new MongoDatabase();
+
+const app = express();
+
+const PORT = 3000;
+
+const __dirname = path.resolve();
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
